7bc81c344d85bcc7643e1216590be45dec45965e
Daniel <danielkonstanty@tukano.com.pl>
2017-12-13 23:12:28 +0100

    dokończenie strony internetowej serwisu

---
+++ Weterynarz.Web/ViewComponents/TopNavbarViewComponent.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Weterynarz.Web.ViewComponents
+{
+    public class TopNavbarViewComponent : ViewComponent
+    {
+        public TopNavbarViewComponent()
+        {
+
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
