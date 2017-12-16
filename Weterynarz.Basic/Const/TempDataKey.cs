529062409e4c5b26c770f89dc0481841feaba84d
Daniel <danielkonstanty@tukano.com.pl>
2017-12-16 01:04:18 +0100

    dodanie tempdatakey

---
+++ Weterynarz.Basic/Const/TempDataKey.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Weterynarz.Basic.Const
+{
+    public static class TempDataKey
+    {
+        public readonly static string Notification;
+
+        static TempDataKey()
+        {
+            Notification = Guid.NewGuid().ToString();
+        }
+    }
+}
