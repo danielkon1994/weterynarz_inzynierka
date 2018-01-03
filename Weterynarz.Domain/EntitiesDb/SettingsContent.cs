be8e1ace7d0e24b48e45985cd631e2a3cb234d9a
Daniel <danielkonstanty@tukano.com.pl>
2018-01-03 17:26:48 +0100

    zmiany w widoku panelu admina dodanie tabeli z ustawieniami treści strony głównej

---
+++ Weterynarz.Domain/EntitiesDb/SettingsContent.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Weterynarz.Domain.EntitiesDb
+{
+    public class SettingsContent : BaseEntity
+    {
+        [Required]
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public string Value { get; set; }
+
+        public string Group { get; set; }
+    }
+}
---
+++ Weterynarz.Domain/Migrations/20180103162111_AddSettingsContentTable.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace Weterynarz.Domain.Migrations
+{
+    public partial class AddSettingsContentTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
